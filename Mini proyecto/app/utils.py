8c2ae5469420734033cd1efe509e2486f978062b
@johan105 <johan@whaleandjaguar.co>
2022-03-13 16:44:33 -0500

    feature: docstring applied to function generic_model_mutation_process

--- Mini proyecto/app/utils.py
+++ Mini proyecto/app/utils.py
@@ -1,4 +1,22 @@
 def generic_model_mutation_process(model, data, id=None, commit=True):
+    '''
+    Method used to create or update objects in db.
+
+    Parameters
+    ----------
+        model : django.db.model
+            model in which the data should be inserted
+        data : Dict
+            enlists the fields that will be used in the model
+        id : int(optional)
+            used to indicate if the process will be an update,
+            and seache the object in db.
+        commit: bool(optional)
+            indicates if the object will be seved on db
+    Returns
+    -------
+        model object seved or updated, or None if get model object went wrong
+    '''
     if id:
         item = model.objects.get(id=id)
         try:
